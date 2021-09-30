f083dfa8788e1c978dc8735ce657e1eaf63852eb
Andrés <andres.plandolit@gmail.com>
2021-09-30 14:26:28 -0300

    nuevas entidades #1

--- migrations/Version20210930160145.php
+++ migrations/Version20210930160145.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210908211645 extends AbstractMigration
+final class Version20210930160145 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,13 @@ final class Version20210908211645 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user CHANGE keycloak_id keycloak_id VARCHAR(500) NOT NULL');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649491914B1 ON user (keycloak_id)');
+        $this->addSql('ALTER TABLE sexo ADD descripcion VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP INDEX UNIQ_8D93D649491914B1 ON user');
-        $this->addSql('ALTER TABLE user CHANGE keycloak_id keycloak_id INT NOT NULL');
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE sexo DROP descripcion');
     }
 }
